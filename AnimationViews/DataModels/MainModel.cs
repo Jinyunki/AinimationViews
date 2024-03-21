61fb635416a36513dc0e15f956bacf1245b8dc26
[github <jinyunki@WIN-8KDN1RUL8VT>
2024-03-21 12:44:49 +0900

    초기 커밋.

---
+++ AnimationViews/DataModels/MainModel.cs
@@ -0,0 +1,31 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AnimationViews.DataModels
+{
+    public class MainModel : ViewModelBase
+    {
+        public ICommand BtnMinmize { get; set; }
+        public ICommand BtnMaxsize { get; set; }
+        public ICommand BtnClose { get; set; }
+        private WindowState _windowState;
+        public WindowState WindowState
+        {
+            get { return _windowState; }
+            set
+            {
+                if (_windowState != value)
+                {
+                    _windowState = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ AnimationViews/ViewModel/MainViewModel.cs
@@ -0,0 +1,67 @@
+using AnimationViews.DataModels;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AnimationViews.ViewModel
+{
+    public class MainViewModel : MainModel
+    {
+        public MainViewModel()
+        {
+            BtnMinmize = new RelayCommand(WinMinmize);
+            BtnMaxsize = new RelayCommand(WinMaxSize);
+            BtnClose = new RelayCommand(WindowClose);
+        }
+
+
+        // Window Minimize
+        private void WinMinmize()
+        {
+            Trace.WriteLine("==========   Start   ==========\nMethodName : " + MethodBase.GetCurrentMethod().Name + "\n");
+            try
+            {
+                WindowState = WindowState.Minimized;
+            } catch (Exception ex)
+            {
+                Trace.WriteLine("========== Exception ==========\nMethodName : " + MethodBase.GetCurrentMethod().Name + "\nException : " + ex);
+                throw;
+            }
+        }
+
+        // Window Size
+        private void WinMaxSize()
+        {
+
+            Trace.WriteLine("==========   Start   ==========\nMethodName : " + MethodBase.GetCurrentMethod().Name + "\n");
+            try
+            {
+                WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+            } catch (Exception ex)
+            {
+                Trace.WriteLine("========== Exception ==========\nMethodName : " + MethodBase.GetCurrentMethod().Name + "\nException : " + ex);
+                throw;
+            }
+
+        }
+        private void WindowClose()
+        {
+
+            Trace.WriteLine("==========   Start   ==========\nMethodName : " + MethodBase.GetCurrentMethod().Name + "\n");
+            try
+            {
+                Application.Current.Shutdown();
+
+            } catch (Exception ex)
+            {
+                Trace.WriteLine("========== Exception ==========\nMethodName : " + MethodBase.GetCurrentMethod().Name + "\nException : " + ex);
+                throw;
+            }
+
+        }
+    }
+}
